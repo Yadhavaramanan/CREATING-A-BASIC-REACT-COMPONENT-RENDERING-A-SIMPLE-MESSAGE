14439e3a7c0bf6eba5691ec15be6c2a515ca05a8
Jayanthan S <144931034+jayanthansenthilkumar@users.noreply.github.com>
2024-08-30 21:19:26 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React
         </a>
       </header>
     </div>
